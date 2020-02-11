380b615b6bb48024330a6df2436a73491a815c5b
Rorywork <rorywork95@outlook.com>
2020-02-11 16:30:15 +0000

    Updates

---
+++ precache-manifest.e85702fa076255ad819b0546c4de4a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9995d017cec2ab40eb5d62bb29758e63",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "9a8cf11739d751bbf66e",
+    "url": "/seasons/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "d5ce41292cd93e9bfec0",
+    "url": "/seasons/static/js/2.c89fdf0a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/seasons/static/js/2.c89fdf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8cf11739d751bbf66e",
+    "url": "/seasons/static/js/main.402da262.chunk.js"
+  },
+  {
+    "revision": "42be413655da1659cb2e",
+    "url": "/seasons/static/js/runtime-main.a8b905c4.js"
+  }
+]);
